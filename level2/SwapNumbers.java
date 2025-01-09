155410246b9a5ca5b4dae38780cd9bf271a7168e
SaurabhKr002 <sourabh21kr@gmail.com>
2025-01-09 23:26:02 +0530

    SwapNumbers_problem7

---
+++ level2/SwapNumbers.java
@@ -0,0 +1,21 @@
+// Importing Scanner class from util pkg for taking input 
+import java.util.Scanner;
+// Creating class for Swapping numbers
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = input.nextInt();
+
+        // Swapping
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+
+        System.out.println("The swapped numbers are " + number1 + " and " + number2);
+    }
+}
